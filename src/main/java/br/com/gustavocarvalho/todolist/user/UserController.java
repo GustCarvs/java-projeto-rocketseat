8c7433e97906f34635806cf2013aa86cf14c39a1
GustCarvs <gustavoccarvalho24@gmail.com>
2025-08-08 18:34:52 -0300

    Criação da Task Controller para lidar com as requisições relacionadas as tarefas

--- src/main/java/br/com/gustavocarvalho/todolist/user/UserController.java
+++ src/main/java/br/com/gustavocarvalho/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.gustavocarvalho.todolist.user;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +9,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+    
+
+
 @RestController  //controla todas as rotas/recursos de uma APIrest
 @RequestMapping("/users")   
 public class UserController {
 
     @Autowired //parte do spring que permite não ter que inicializar classes
     private IUserRepository userRepository;
+
+    
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel) {
@@ -22,6 +29,13 @@ public class UserController {
         if(user != null){ //se a condição for true, o usuario ja existe
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwodHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray()); //coleta a senha do usuario e faz a verificação
+
+        userModel.setPassword(passwodHashred);//set a password com o que o user forneceu 
+        
+
+
         var userCreated = this.userRepository.save(userModel);
             return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
