e7e46aa3597f51f417bb04a6101711904ee36d7f
GustCarvs <gustavoccarvalho24@gmail.com>
2025-09-18 21:17:28 -0300

    validando se o usuario ja existe dentro da aplicação

--- src/main/java/br/com/gustavocarvalho/todolist/Filter/FilterTaskAuth.java
+++ src/main/java/br/com/gustavocarvalho/todolist/Filter/FilterTaskAuth.java
@@ -4,50 +4,70 @@ import java.io.IOException;
 import java.util.Base64;
 
 import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.gustavocarvalho.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component //utiliza quando desejar que o spring gerencia a clase 
-public class FilterTaskAuth extends OncePerRequestFilter{  //filter do spring ja com os tipos http
+@Component // utiliza quando desejar que o spring gerencia a clase
+public class FilterTaskAuth extends OncePerRequestFilter { // filter do spring ja com os tipos http
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-                //pegar a autenticação  
-                var authorization = request.getHeader("Authorization");
+        var servletPath = request.getServletPath();
+
+        if (servletPath.equals("/tasks/")) {
+            // pegar a autenticação
+            var authorization = request.getHeader("Authorization");
+
+            var authEncoded = authorization.substring("Basic".length()).trim(); // substring: um parâmetro - o programa
+                                                                                // "corta" o primeiro parâmetro
+                                                                                // fornecido
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authDecode);
+
+            // ["guga", "12345"]
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            System.out.println("Authorization");
+            System.out.println(username);
+            System.out.println(password);
+
+            // validar o usuário
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                // validar senha
+                var passworVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passworVerify.verified) {
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+                // executa a api
                 
-                var authEncoded = authorization.substring("Basic".length()).trim(); //substring: um parâmetro - o programa "corta" o primeiro parâmetro fornecido 
+            }
 
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+        }else{
+            filterChain.doFilter(request, response);    
+        }
 
-                var authString = new String(authDecode);
-
-                
-                // ["guga", "12345"]
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
-                
-                System.out.println("Authorization");
-                System.out.println(username);
-                System.out.println(password);
-                
-                //validar o usuário 
-
-                //validar senha 
-
-                //executa a api 
-
-                filterChain.doFilter(request, response);  
-       
     }
-
 
 }
